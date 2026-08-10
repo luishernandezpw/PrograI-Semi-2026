8b0604d59b03ef70b5e212b349266415cd4d007d
Luis Hernandez <sarco_luis@hotmail.com>
2026-08-10 09:34:42 -0600

    uso de validaciones en la edad

--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
@@ -17,23 +17,27 @@ namespace miPrimeaAplicacion {
 
         }
         private void btnCalcular_Click(object sender, EventArgs e) {
-            int edad = int.Parse(txtEdad.Text);
-            if (edad < 0) {
-                lblRespuesta.Text = "Edad no valida";
-            } else if (edad <=2) {
-                lblRespuesta.Text = "Eres un bebe";
-            } else if(edad<12){
-                lblRespuesta.Text = "Eres un niño";
-            }else if (edad <= 17) {
-                lblRespuesta.Text = "Eres un adolescente";
-            }else if (edad < 65) {
-                lblRespuesta.Text = "Eres un adulto";
-            }else if (edad <= 80) {
-                lblRespuesta.Text = "Eres un adulto mayor";
-            }else if (edad <= 130) {
-                lblRespuesta.Text = "Larga vidad";
-            } else {
-                lblRespuesta.Text = "Edad no valida";
+            try {
+                int edad = int.Parse(txtEdad.Text);
+                if (edad < 0) {
+                    lblRespuesta.Text = "Edad no valida";
+                } else if (edad <= 2) {
+                    lblRespuesta.Text = "Eres un bebe";
+                } else if (edad < 12) {
+                    lblRespuesta.Text = "Eres un niño";
+                } else if (edad <= 17) {
+                    lblRespuesta.Text = "Eres un adolescente";
+                } else if (edad < 65) {
+                    lblRespuesta.Text = "Eres un adulto";
+                } else if (edad <= 80) {
+                    lblRespuesta.Text = "Eres un adulto mayor";
+                } else if (edad <= 130) {
+                    lblRespuesta.Text = "Larga vidad";
+                } else {
+                    lblRespuesta.Text = "Edad no valida";
+                }
+            }catch(Exception) {
+                lblRespuesta.Text = "Por favo ingrese la edad en numero entero";
             }
         }
     }
