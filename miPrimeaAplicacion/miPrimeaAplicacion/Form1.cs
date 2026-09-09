a1d36c8d64b4e23d2c2eaf8fde01f1dcdc35a76c
Luis Hernandez <sarco_luis@hotmail.com>
2026-09-09 09:57:37 -0600

    Creacion de la clase Estadistica paras separar responsabilidades y reutilizacion de codigo, paradigma POO

--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
@@ -13,36 +13,18 @@ namespace miPrimeaAplicacion {
         public Form1() {
             InitializeComponent();
         }
-        double media(double[] serie){
-            double suma = 0;
-            for (int i = 0; i < serie.Length; i++){
-                suma += serie[i];
-            }
-            double media = suma / serie.Length;
-            return media;
-        }
-        double varianza(double[] serie, double media) {
-            return serie.Average(n => Math.Pow(n - media, 2));
-        }
-        double desviacionTipica(double[] serie, double media){
-            return Math.Sqrt(varianza(serie, media));
-        }
-        double armonica(double[] serie) {
-            int n = serie.Length;
-            return n / serie.Sum(x=>1/x);
-        }
-
+        Estadistica objEsta = new Estadistica();
         private void btnProcesar_Click(object sender, EventArgs e)
         {
             limpiar();
             String[] serie = txtSerie.Text.Split(',');
             double[] miSerie = serie.Select(n=>double.Parse(n)).ToArray();
-            double m = media(miSerie);
+            double m = objEsta.media(miSerie);
 
             ltsValores.Items.Add("La media es: " + m);
-            ltsValores.Items.Add("La desviacion estandar: " + varianza(miSerie, m));
-            ltsValores.Items.Add("La desviacion tipica: "+ desviacionTipica(miSerie, m));
-            ltsValores.Items.Add("La media armonica: " + armonica(miSerie));
+            ltsValores.Items.Add("La desviacion estandar: " + objEsta.varianza(miSerie, m));
+            ltsValores.Items.Add("La desviacion tipica: "+ objEsta.desviacionTipica(miSerie, m));
+            ltsValores.Items.Add("La media armonica: " + objEsta.armonica(miSerie));
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e) {
